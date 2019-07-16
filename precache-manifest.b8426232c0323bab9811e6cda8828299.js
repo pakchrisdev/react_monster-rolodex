647bca65c6c4238393067cfe852ad78eea70c02f
christopher pak <macbookpro@christophersMBP.fios-router.home>
2019-07-16 03:31:22 -0400

    Updates

---
+++ precache-manifest.b8426232c0323bab9811e6cda8828299.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2369b1224d052a94351b73564213cb0",
+    "url": "/react_monster-rolodex/index.html"
+  },
+  {
+    "revision": "0c17ab3bd097ce397f5b",
+    "url": "/react_monster-rolodex/static/css/main.48c01d64.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react_monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "0c17ab3bd097ce397f5b",
+    "url": "/react_monster-rolodex/static/js/main.7fc4cc81.chunk.js"
+  },
+  {
+    "revision": "fee351e52002bbca3a25",
+    "url": "/react_monster-rolodex/static/js/runtime~main.58059295.js"
+  }
+]);
